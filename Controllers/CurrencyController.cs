9d090d8766a41dbfd650168e304047bb63659d88
mahinfah <mahinfah1234@gmail.comgit config --global user.email mahinfah1234@gmail.com>
2026-07-14 02:32:17 +0600

    added Views/Currency/Index.cshtml

--- Controllers/CurrencyController.cs
+++ Controllers/CurrencyController.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Interview_task.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Interview_task.Controllers
 {
     public class CurrencyController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(new CurrencyViewModel());
         }
     }
 }
