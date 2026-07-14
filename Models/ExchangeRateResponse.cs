a89a7c126a765867161d3fb9b95d596b44cde7a7
mahinfah <mahinfah1234@gmail.comgit config --global user.email mahinfah1234@gmail.com>
2026-07-14 13:56:30 +0600

    Added ExchnageRateresponse

--- Models/ExchangeRateResponse.cs
+++ Models/ExchangeRateResponse.cs
@@ -1,6 +1,19 @@
-﻿namespace Interview_task.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Interview_task.Models
 {
     public class ExchangeRateResponse
     {
+        [JsonPropertyName("amount")]
+        public decimal Amount { get; set; }
+
+        [JsonPropertyName("base")]
+        public string Base { get; set; } = "";
+
+        [JsonPropertyName("date")]
+        public string Date { get; set; } = "";
+
+        [JsonPropertyName("rates")]
+        public Dictionary<string, decimal> Rates { get; set; } = new();
     }
 }
--- Services/CurrencyService.cs
+++ Services/CurrencyService.cs
@@ -1,6 +1,36 @@
-﻿namespace Interview_task.Services
+﻿using System.Text.Json;
+using Interview_task.Models;
+
+namespace Interview_task.Services
 {
     public class CurrencyService
     {
+        private readonly HttpClient _httpClient;
+
+        public CurrencyService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ExchangeRateResponse?> ConvertCurrency(
+            decimal amount,
+            string from,
+            string to  )
+        {
+            string url =
+                $"https://api.frankfurter.app/latest?amount={amount}&from={from}&to={to}";
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            string json =
+                await response.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<ExchangeRateResponse>(json);
+        }
     }
 }
